5e19940c183160af89177c6116057ff3bc306328
Md Aman <72139227+mdaman999@users.noreply.github.com>
2025-03-11 01:56:46 +0530

    add one more line2

--- Main.java
+++ Main.java
@@ -2,5 +2,6 @@ public class Main{
     public static void main(String args[]){
         System.out.println("Hello,World!");
         System.err.println("bye-bye");
+ 	System.err.println("Thanks");
     }
 }
